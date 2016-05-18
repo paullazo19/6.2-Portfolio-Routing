03450b8d983c86e3da41b8608235ff7ee0f3b1e8
Paul Lazo <paullazo19@gmail.com>
2016-05-18 09:59:24 -0500

    coupled project titles with links to respective projects

--- modules/Etsy.js
+++ modules/Etsy.js
@@ -4,8 +4,10 @@ export default React.createClass({
   render(){
     return(
       <section>{this.props.params.Etsy}
-        <h1>Etsy Project</h1>
-        <a href="http://paullazo19-etsy-clone.surge.sh/">Try out my Etsy clone!</a>
+        <div>
+          <h1>Etsy Project</h1>
+          <a href="http://paullazo19-etsy-clone.surge.sh/">Try out my Etsy clone!</a>
+        </div>
         <img className="image" src="/assets/etsy.png"/>
 
 
